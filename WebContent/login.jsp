1983332a40b4f12070b44fd3e4b0a4d20c89f39f
Sivangi99 <sivangitandon@gmail.com>
2020-06-14 23:10:43 +0530

    Error 404

---
+++ WebContent/login.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title> Login </title>
+</head>
+<body>
+<h1>Login Page</h1>
+<form name="loginform" action="login" method="post">
+<table>
+<tr>
+<td>User ID</td>
+<td><input type="text" name="na"></td>
+</tr>
+<tr>
+<td>Password</td>
+<td><input type="password" name="psw"></td>
+</tr>
+<tr>
+<td><input type="submit" name="sub" value="Login"></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
--- src/com/yourbank/login/Login.java
+++ src/com/yourbank/login/Login.java
@@ -1,7 +1,7 @@
 package com.yourbank.login;
 
 import java.io.IOException;
-
+import java.sql.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,16 +9,47 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.yourbank.constants.Utilities;
 
-@WebServlet(value = "/login")
+//@WebServlet(value = "/login")
 public class Login extends HttpServlet {
 
 	public void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException
+	{ try{
+		String userId = req.getParameter("na");
+		String password = req.getParameter("psw");
+		//System.out.println("Reached here");
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/bank","root","");
+		String sql="select * from employee where UserId = ' "+userId+" '";
+		//String sql="insert into employee values("+userId+",' "+password+" ')";
+		Statement st=con.createStatement();
+		ResultSet rs=st.executeQuery(sql);
+		int flag=0;
+		
+		while(rs.next())
+		{
+			flag++;
+		}
+		if(flag==0)
+			res.getWriter().println("Invalid username");
+		else{
+			sql="select * from Employee where Password= ' "+password+" '";
+			st=con.createStatement();
+			rs=st.executeQuery(sql);
+			while(rs.next()){
+				flag++;
+			}
+			if(flag<2)
+				res.getWriter().println("Invalid Password");
+			else{
+				res.getWriter().println("Logged in");//res.sendRidirect("loginsuccess.jsp");
+			}
+		}
+		
+	}catch(Exception e)
 	{
-		String userId = req.getParameter(Utilities.USERID);
-		String password = req.getParameter(Utilities.PASSWORD);
-		
-		res.getWriter().println("Login works!");
-		
+		System.err.println(e);
+	}
+	//res.getWriter().println("Login works!");}
 		//search for userid in database
 		//if not found, display userid not found error msg
 		//if found, compare password.
